8f8b33603712675fc87d552409c77cbe968ad2e4
alexcwu1121 <alexcwu1121@gmail.com>
2025-11-23 18:02:46 -0500

    change

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # yocto-hammerhead
+
+sudo rpiboot
+
+sudo dd if=/dev/zero of=/dev/sda bs=4M count=10
+sync
+
+sudo bzcat core-image-hammerhead-hammerhead.rootfs.wic.bz2 | sudo dd of=/dev/sda bs=4M status=progress conv=fsync
+sync
--- setup_env.sh
+++ setup_env.sh
@@ -7,4 +7,4 @@ WORKDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 podman build . -f Dockerfile.yocto-env-ubuntu-22 -t yocto-env-ubuntu-22
 
 # Open an interactive terminal
-podman run --userns=keep-id --rm -it -v ${WORKDIR}:${WORKDIR}:Z -w ${WORKDIR} yocto-env-ubuntu-22:latest
+podman run --userns=keep-id --rm -it --privileged -v ${WORKDIR}:${WORKDIR}:Z -v /dev/bus/usb:/dev/bus/usb -w ${WORKDIR} yocto-env-ubuntu-22:latest
