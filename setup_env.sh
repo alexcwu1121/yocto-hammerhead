5e8d73afd003af6fb8d8d91c9b850315a60374c2
alexcwu1121 <alexcwu1121@gmail.com>
2026-09-14 20:25:04 -0400

    make podman run multi-line

--- setup_env.sh
+++ setup_env.sh
@@ -7,4 +7,10 @@ WORKDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 podman build . -f Dockerfile.yocto-env-ubuntu-22 -t yocto-env-ubuntu-22
 
 # Open an interactive terminal
-podman run --userns=keep-id --rm -it --privileged -v ${WORKDIR}:${WORKDIR}:Z -v /dev/bus/usb:/dev/bus/usb -w ${WORKDIR} yocto-env-ubuntu-22:latest
+podman run \
+    --userns=keep-id \
+    --privileged \
+    --rm -it \
+    -v ${WORKDIR}:${WORKDIR} \
+    -v /dev/bus/usb:/dev/bus/usb \
+    -w ${WORKDIR} yocto-env-ubuntu-22:latest
